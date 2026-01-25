dd2b1e10f2ce53abcbe0a4f1c14bd6670b03a83a
thatjuan <juan@cxdmail.com>
2026-01-24 20:47:13 -0500

    chore: disable GCP Pub/Sub resources
    GCP Pub/Sub topic and subscription for Gmail integration are not
    currently needed. Comment out:
    - GCP config in all stack files (gcp_project, gcp:project, gcp:region)
    - Gmail webhook config (endpoint, secret)
    - create_pubsub_resources import and usage in code
    
    Resources can be re-enabled by uncommenting the relevant sections.

--- infrastructure/__init__.py
+++ infrastructure/__init__.py
@@ -1,12 +1,13 @@
 """Infrastructure modules for Clustera platform."""
 
-from .integrations import create_kafka_resources, create_pubsub_resources
+from .integrations import create_kafka_resources
+# from .integrations import create_pubsub_resources  # GCP Pub/Sub disabled
 from .data_plane import create_data_plane_kafka_resources
 from .core.cloudflare import create_cloudflare_dns_records
 
 __all__ = [
     "create_kafka_resources",
-    "create_pubsub_resources",
+    # "create_pubsub_resources",  # GCP Pub/Sub disabled
     "create_data_plane_kafka_resources",
     "create_cloudflare_dns_records",
 ]
